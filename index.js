bf4633e2c64594d1f00536df8c9d4d5bd2e7a21e
SaurabhSinha77329 <sin.sau1@gmail.com>
2019-06-12 23:51:41 +0530

    added atlas

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require('express')
-const parser = require('body-parser')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
 
 const port = 3000;
+
+mongoose.connect("mongodb+srv://Razr7:batman123@cluster0-g0pwk.mongodb.net/test?retryWrites=true&w=majority",function(err){
+    if(err){
+        console.log(err);
+    }
+    else
+    console.log("Atlas Connected");
+})
+
+
+//importing routes...........................................................
+const placeRoutes = require('./routes/place');
+
+
+
+//middlewares
+app.use(bodyParser.json());
+
+app.use('/place',placeRoutes);
+
+
+
+
+//server......................................................................
 
 app.listen(port,function(){
     console.log(`Server listening on ${port}`)
