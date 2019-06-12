93db19d18f9e957245a9e37152056452a817ea96
SaurabhSinha77329 <sin.sau1@gmail.com>
2019-06-12 19:53:35 +0530

    models of database

---
+++ models/hostModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const hostSchema = mongoose.Schema({
+    _id : mongoose.Schema.Types.ObjectId,
+    name : {type:String,required:true},
+    email : {type:String,required:true,unique:true},
+    password : {type:String,required:true},
+    phoneno : {type:Number,required:true},
+    Curevent : {type:Array,required:false}
+})
+
+module.exports = mongoose.model('Host',hostSchema);
---
+++ models/placeModel.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const placeSchema = mongoose.Schema({
+    _id : mongoose.Schema.Types.ObjectId,
+    location : {type:String,required:true},
+    sport : {type:String,required:true},
+
+})
+
+module.exports = mongoose.model('Place',placeSchema);
