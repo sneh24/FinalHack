1435a538ceee4e89b897dd3a4d3315dd502e9f0c
greed4 <bhaskar4uday@gmail.com>
2019-06-12 22:45:34 +0530

    placeRoutes

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express')
-const parser = require('body-parser')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
 
 const port = 3000;
+
+
+
+
+//importing routes...........................................................
+const placeRoutes = require('./routes/place');
+
+
+
+//middlewares
+app.use(bodyParser.json());
+
+app.use('/place',placeRoutes);
+
+
+
+
+//server......................................................................
 
 app.listen(port,function(){
     console.log(`Server listening on ${port}`)
