447281ac17e85b6e15468f77a0ad4e3e3fc7040a
greed4 <bhaskar4uday@gmail.com>
2019-06-13 19:26:19 +0530

    integration

--- routes/user.js
+++ routes/user.js
@@ -13,13 +13,13 @@ router.get('/register',function(req,res,next){
     res.render('register');
 })
 
+
+//user login page---------------------------------------------------------------------
 router.get('/login',function(req,res,next){
     res.render('index');
 })
 
-router.get('/hostlogin',function(req,res,next){
-    res.render('loginHost');
-})
+
 
 
 
