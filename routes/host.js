73f62d927ba8ec6aa5a45b77e225e2b92bbff573
greed4 <bhaskar4uday@gmail.com>
2019-06-13 15:10:03 +0530

    addEvents

--- routes/host.js
+++ routes/host.js
@@ -4,8 +4,11 @@ const mongoose = require('mongoose')
 
 //import local modules----------------------------------------------------------
 const hostModel = require('../models/hostModel');
+const eventModel = require('../models/eventsModel');
 
 
+
+//host register------------------------------------------------------------------
 router.post('/register',function(req,res,next){
     const newHost = new hostModel({
         _id: new mongoose.Types.ObjectId(),
@@ -30,6 +33,38 @@ router.post('/register',function(req,res,next){
     .catch(next);
 })
 
+
+
+//host adding events ---------------------------------------------------------------
+router.post('addevent/:hostid',function(req,res,next){
+    const newEvent = new eventModel({
+        _id : new mongoose.Types.ObjectId(),
+        host: req.params.hostid,
+        place: req.body.place,
+        sport: req.body.sport,
+        date:req.body.date,
+        capacity:req.body.capacity,
+        count:req.body.count
+    })
+
+    eventModel.find({date:req.body.date,place:req.body.place,sport:req.body.sport})
+    .exec()
+    .then((event)=>{
+        if(event.length>0)
+        {
+            res.send("ON GIVEN DATE EVENT EXISTS").status(200);
+        }
+        else{
+            newEvent.save()
+            .then(()=>{
+                res.send("event added");
+            })
+            .catch(next);
+
+        }
+    })
+    .catch(next);
+})
 
 
 module.exports = router;
