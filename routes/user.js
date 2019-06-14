644e1c1d93cd4962c1d4597da7ab9dc6b721a109
greed4 <bhaskar4uday@gmail.com>
2019-06-14 19:33:19 +0530

    alluser

--- routes/user.js
+++ routes/user.js
@@ -110,7 +110,7 @@ router.get('/getevents/:sport',function(req,res,next){
 
 //get the list of all user------------------------------------------------------------------
 router.get('/alluser',function(req,res,next){
-    hostModel.find({})
+    userModel.find({})
     .exec()
     .then((user=>{
          res.send(user);
