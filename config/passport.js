9324d7f66e2c0be8d5431cfe0a6305f7a3272b06
greed4 <bhaskar4uday@gmail.com>
2019-06-14 12:43:35 +0530

    login

--- config/passport.js
+++ config/passport.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const User = require('../models/userModel');
+const Host = require('../models/hostModel');
 
-module.exports = function(passport) {
+module.exports =  function(passport) {
     passport.use(
         new LocalStrategy({ usernameField : 'email'}, (email,password,done) => {
             User.findOne({ email: email })
@@ -38,3 +39,9 @@ module.exports = function(passport) {
         });
       });
 }
+
+
+
+
+
+
