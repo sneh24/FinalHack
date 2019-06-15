e1617a91f5e57ec0e8957a233bc0497f5e9f20a0
kingzilla <asharmessi96@gmail.com>
2019-06-15 15:07:35 +0530

    Join with payment done

--- routes/user.js
+++ routes/user.js
@@ -184,11 +184,12 @@ router.get('/alluser',function(req,res,next){
 
 //on click join button on home user page-----------------------------------------------------------
 router.get('/joinpage/:eventid',ensureAuthenticated,function(req,res,next){
-    // eventModel.findOne({_id:req.params.eventid})
-    // .then((event)=>{
-        //console.log(event);
-        res.render('join',{eventid:req.params.eventid});
-    //})
+     eventModel.find({_id:req.params.eventid})
+     .then((event)=>{
+         console.log("Inside then");
+        console.log(event);
+        res.render('join',{events:event});
+    })
     
 })
 
