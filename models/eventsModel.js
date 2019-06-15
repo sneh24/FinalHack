752d997357a8615c4c5a3201fb63ad3ed7d3d18e
SaurabhSinha77329 <sin.sau1@gmail.com>
2019-06-15 15:17:42 +0530

    new event model

--- models/eventsModel.js
+++ models/eventsModel.js
@@ -6,16 +6,13 @@ const eventSchema = mongoose.Schema({
         ref : 'Host',
         required : true
     },
-    place :{
-        type: mongoose.Schema.Types.ObjectId,
-        ref : 'Place',
-        required : true
-    },
+    place : { type:String,required:true},
     sport :{type:String,required:true},
     date : {type:Date,required:true},
     user : {type:Array,required:false},
     capacity : {type:Number,required:true},
-    count : {type:Number,required:true}
+    count : {type:Number,required:true},
+    status : {type:Boolean,required:true}
 })
 
 module.exports = mongoose.model('Event',eventSchema);
--- models/placeModel.js
+++ models/placeModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const placeSchema = mongoose.Schema({
     _id : mongoose.Schema.Types.ObjectId,
     location : {type:String,required:true},
-    sport : {type:String,required:true},
+    sport : {type:String,required:true}
 
 })
 
